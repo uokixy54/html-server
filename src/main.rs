d6888dae6e8a8da70e294aad0c8ed8b141fd7f4a
uokixy54 <uokixy.lemberry@gmail.com>
2026-03-12 01:23:40 +0900

    ⚗️ 永続的にリクエストを受け付けるようにloopを追加

--- src/main.rs
+++ src/main.rs
@@ -23,20 +23,22 @@ fn main() {
         let listen_result = listen(socket_fd, 128);
         if listen_result < 0 { panic!("listen error"); }
 
-        // accept
-        let client_fd = accept(socket_fd, std::ptr::null_mut(), std::ptr::null_mut());
+        loop {
+            // accept
+            let client_fd = accept(socket_fd, std::ptr::null_mut(), std::ptr::null_mut());
 
-        // get http request
-        let mut buffer = [0u8; 1024];
-        let read_bytes = read(client_fd, buffer.as_mut_ptr() as *mut c_void, buffer.len());
-        if read_bytes < 0 { panic!("read error"); }
-        println!("read_bytes: {}", std::str::from_utf8(&buffer[..read_bytes as usize]).unwrap());
+            // get http request
+            let mut buffer = [0u8; 1024];
+            let read_bytes = read(client_fd, buffer.as_mut_ptr() as *mut c_void, buffer.len());
+            if read_bytes < 0 { panic!("read error"); }
+            println!("read_bytes: {}", std::str::from_utf8(&buffer[..read_bytes as usize]).unwrap());
 
-        // return http response
-        let message = String::from("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!");
-        println!("message: {}", &message);
-        let write_bytes = write(client_fd, message.as_bytes().as_ptr() as *const c_void, message.len());
-        if write_bytes < 0 { panic!("write to client file descriptor filed"); }
-
+            // return http response
+            let message = String::from("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!");
+            println!("message: {}", &message);
+            let write_bytes = write(client_fd, message.as_bytes().as_ptr() as *const c_void, message.len());
+            if write_bytes < 0 { panic!("write to client file descriptor filed"); }
+        }
+        
     }
 }
